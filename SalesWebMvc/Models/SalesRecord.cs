39691c35d95be47db9d9fedf0f46bdec33cdc469
kennedy <kennedymnovas@gmail.com>
2019-08-05 00:38:42 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public Seller Seller { get; set; }
+        public SaleStatus Status { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, Seller seller, SaleStatus status, DateTime date, double amount)
+        {
+            Id = id;
+            Seller = seller;
+            Status = status;
+            Date = date;
+            Amount = amount;
+        }
+    }
+}
