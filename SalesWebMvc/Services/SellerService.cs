19e1544a3bbc20810bf12f94f830c4e827b8eb6e
kmvbatista <kennedymnovas@gmail.com>
2019-08-18 23:05:38 -0300

    Deleting an item

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -25,5 +25,16 @@ namespace SalesWebMvc.Services
             _context.Seller.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            _context.Seller.Remove(this.FindById(id));//removi do dbSet, falta saveChanges pra alterar no banco
+            _context.SaveChanges();
+        }
     }
 }
